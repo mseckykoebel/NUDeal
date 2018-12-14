fbea65d50fee75254e35378316549c74d2d19819
Mason Secky-Koebel <mseckykoebel@gmail.com>
2018-12-14 11:35:10 -0600

    Finished the property cards, houses, hotels, and property wild cards in GameState and added new params to PropertyCard.

--- app/src/main/java/com/example/jameswilliams/nu_deal/nu_deal_game/GameState.java
+++ app/src/main/java/com/example/jameswilliams/nu_deal/nu_deal_game/GameState.java
@@ -86,29 +86,70 @@ public class GameState
 
         //Red Properties
         allCards.add(new PropertyCard("KentuckyAvenue", "Red",3 ));
+        allCards.add(new PropertyCard("IndianaAvenue", "Red", 3));
+        allCards.add(new PropertyCard("IllinoisAvenue","Red",3));
 
         //Yellow Properties
+        allCards.add(new PropertyCard("AtlanticAvenue","Yellow",3));
+        allCards.add(new PropertyCard("VentnorAvenue","Yellow",3));
+        allCards.add(new PropertyCard("MarvinGardens","Yellow",3));
 
         //Light Blue Properties
+        allCards.add(new PropertyCard("ConnecticutAvenue","SkyBlue",1));
+        allCards.add(new PropertyCard("VermontAvenue","SkyBlue",1));
+        allCards.add(new PropertyCard("OrientalAvenue","SkyBlue",1));
 
         //Brown Properties
+        allCards.add(new PropertyCard( "BalticAvenue", "SaddleBrown", 1));
+        allCards.add(new PropertyCard("MediterraneanAvenue","SaddleBrown",1));
 
         //Orange Properties
+        allCards.add(new PropertyCard("StJamesPlace","Orange", 2));
+        allCards.add(new PropertyCard("NewYorkAvenue","Orange",2));
+        allCards.add(new PropertyCard("TennesseeAvenue","Orange",2));
 
-        //Purple Properties
+        //Purple Properties (these are DarkOrchid !!!!!!)
+        allCards.add(new PropertyCard("StCharlesPlace","DarkOrchid",2));
+        allCards.add(new PropertyCard("VirginiaAvenue","DarkOrchid",2));
+        allCards.add(new PropertyCard("StatesAvenue","DarkOrchid", 2));
 
-        //Utilities
+        // Utilities
+        // Color group is grey
+        allCards.add(new PropertyCard( "ElectricCompany", "Grey", 2));
+        allCards.add(new PropertyCard( "WaterWorks", "Grey", 2));
 
         //Railroads
+        allCards.add(new PropertyCard("ShortLine", "Black",2 ));
+        allCards.add(new PropertyCard("ReadingRailroad", "Black",2));
+        allCards.add(new PropertyCard("B&ORailroad","Black",2));
+        allCards.add(new PropertyCard("PennsylvaniaRailroad","Black",2));
 
         //Two Type Wild Cards
+        allCards.add(new PropertyCard("RedYellow1","Red","Yellow",3));
+        allCards.add(new PropertyCard("RedYellow2","Red","Yellow",3));
+
+        allCards.add(new PropertyCard("RailroadSkyBlue","Black","SkyBlue",4));
+        allCards.add(new PropertyCard("RailRoadGrey","Black","Grey",4));
+
+        allCards.add(new PropertyCard("OrangeDarkOrchid1","Orange","DarkOrchid",2));
+        allCards.add(new PropertyCard("OrangeDarkOrchid2","Orange","DarkOrchid",2));
+
+        allCards.add(new PropertyCard("BlueGreen","Blue","Green",4));
+
+        allCards.add(new PropertyCard("SkyBlueSaddleBrown","SkyBlue","SaddleBrown",1));
 
         //Wild Cards
+        allCards.add(new PropertyCard("WildCard1","",0));
+        allCards.add(new PropertyCard("WildCard2","",0));
 
         //Houses
+        allCards.add(new PropertyCard("House1",3));
+        allCards.add(new PropertyCard("House2",3));
+        allCards.add(new PropertyCard("House3",3));
 
         //Hotels
-
+        allCards.add(new PropertyCard("Hotel1",4));
+        allCards.add(new PropertyCard("Hotel2",4));
 
     }
 
