e618a60fee5d5a81a29be66fdbd64f6cbe6d602b
Mason Secky-Koebel <mseckykoebel@gmail.com>
2018-12-14 12:18:49 -0600

    Finished the property cards, houses, hotels, and property wild cards in GameState and added new params to PropertyCard.

--- app/src/main/java/com/example/jameswilliams/nu_deal/nu_deal_game/PropertyCard.java
+++ app/src/main/java/com/example/jameswilliams/nu_deal/nu_deal_game/PropertyCard.java
@@ -14,11 +14,21 @@ public class PropertyCard extends Card
         this.bankable = false;
     }
 
+    // Property WildCards taking on two color values, and can be banked
     public PropertyCard(String n, String c1, String c2, int v)
     {
         this.name = n;
         this.color1 = c1;
         this.color2 = c2;
+        this.value = v;
+        this.bankable = true;
+    }
+
+    // Houses and Hotels that Can be banked, and qualify as property cards
+    // More logic dealing with these will be done later
+    public PropertyCard(String n, int v)
+    {
+        this.name = n;
         this.value = v;
         this.bankable = true;
     }
