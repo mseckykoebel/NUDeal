c170f323b0cd59995b3156686b7d72ae2dd7a966
Mason Secky-Koebel <mseckykoebel@gmail.com>
2019-01-25 22:19:10 -0600

    Added logic for the progress bar on the button

--- app/src/main/java/com/example/jameswilliams/nu_deal/LoginActivity.java
+++ app/src/main/java/com/example/jameswilliams/nu_deal/LoginActivity.java
@@ -2,12 +2,30 @@ package com.example.jameswilliams.nu_deal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class LoginActivity extends AppCompatActivity {
+
+    // Adding a log in button and a progress rotation logic to the activity
+    Button loginButton;
+    ProgressBar loginProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginButton = findViewById(R.id.login_button);
+        loginProgress = findViewById(R.id.login_progress);
+        loginProgress.setVisibility(View.INVISIBLE);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                loginProgress.setVisibility(View.VISIBLE);
+                loginButton.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
